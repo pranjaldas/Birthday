05053b6d5318d86899a288f912325c0021a59a29
Akrur Ranjan Borah <akrurborah@ymail.com>
2019-09-18 01:03:17 +0530

    Added Splash Screen with GIF loader. Added volume maximizer

--- app/src/main/java/android/example/birthday/Splash.java
+++ app/src/main/java/android/example/birthday/Splash.java
@@ -1,22 +1,48 @@
 package android.example.birthday;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.medialablk.easygifview.EasyGifView;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        EasyGifView easyGifView = findViewById(R.id.easyGifView);
+        easyGifView.setGifFromResource(R.mipmap.bday);
+
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-
+                synchronized (this) {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
             }
-        })
-                .start();
+        }).start();
+
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (audioManager != null) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 20, 0);
+        }
     }
 }
